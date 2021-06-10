bab7b42095aa445c1d2731a5b4c8ce2c982c3355
Chris Raley <craley@doubledutch.me>
2021-06-10 10:09:13 -0700

    Added singleton dialog logic

--- app/src/main/java/com/example/dialogfragmentfun/MainActivity.kt
+++ app/src/main/java/com/example/dialogfragmentfun/MainActivity.kt
@@ -53,7 +53,12 @@ class NonCancellableDialogFragment : DialogFragment() {
         private const val TAG = "NonCancellableDialogFragment"
 
         fun show(fragmentManager: FragmentManager) {
-            NonCancellableDialogFragment().showNow(fragmentManager, TAG)
+            Log.d(LOG_TAG, "NonCancellableDialogFragment.show()")
+            val dialog = (fragmentManager.findFragmentByTag(TAG) as? NonCancellableDialogFragment)
+            if (dialog == null) {
+                Log.d(LOG_TAG, "NonCancellableDialogFragment not present, creating and showing dialog")
+                NonCancellableDialogFragment().showNow(fragmentManager, TAG)
+            }
         }
 
         fun dismiss(fragmentManager: FragmentManager) {
